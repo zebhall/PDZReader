782448c43a717f44cd3358b3186f95d8bbb0a38d
zeb <81948605+zebhall@users.noreply.github.com>
2023-12-19 12:03:17 +0800

    got nowhere

--- SpectraChecking.py
+++ SpectraChecking.py
@@ -5,31 +5,56 @@
 import numpy as np
 from PDZreader import PDZFile
 
-# from functools import wraps
-# import time
-# def timeit(func):
-#     @wraps(func)
-#     def timeit_wrapper(*args, **kwargs):
-#         start_time = time.perf_counter()
-#         result = func(*args, **kwargs)
-#         end_time = time.perf_counter()
-#         total_time = end_time - start_time
-#         print(f"Function {func.__name__} Took {total_time:.4f} seconds")
-#         return result
-#     return timeit_wrapper
+from functools import wraps
+import time
+
+
+def timeit(func):
+    @wraps(func)
+    def timeit_wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        print(f"Function {func.__name__} Took {total_time:.4f} seconds")
+        return result
+
+    return timeit_wrapper
 
 
 # @timeit
 def sanityCheckSpectrum(
-    spectrum_counts: list, spectrum_energies: list, source_voltage_in_kV: int
+    spectrum_counts: list,
+    spectrum_energies: list,
+    source_voltage_in_kV: int,
+    spectrum_live_time_in_s: float,
 ) -> bool:
     """Checks that a spectrum is sensible, and that the listed voltage is accurate. This is required because of a bug in Bruker pXRF instrument software, sometimes causing phases of an assay to use an incorrect voltage. Returns TRUE if sanity check passed, return FALSE if not."""
-    # Calculate the standard deviation of the spectrum
-    std_dev = np.std(spectrum_counts)
-    # print(f"spectrum std dev = {std_dev}")
 
     # Set a threshold for noise detection (too small might be prone to noise, too high isn't useful. starting with stddev/100.)
+    std_dev = np.std(spectrum_counts)
     threshold = std_dev / 50
+    # noise_mean = np.mean(spectrum_counts[-20:])
+    # print(f"{noise_mean=}, {spectrum_live_time_in_s=}")
+    # if noise_mean < 2:
+    #     threshold = 2 * spectrum_live_time_in_s
+    # elif noise_mean > spectrum_live_time_in_s:
+    #     threshold = spectrum_live_time_in_s
+    # else:
+    #     threshold = spectrum_live_time_in_s
+
+    # trying a new method of threshold calculation 20231219, using the last few values of the list to get the 'noise std dev', then adding it to the last channel counts value
+    # std_dev_noise = np.std(spectrum_counts[-10:])
+    # print(f"{std_dev_noise=}")
+    # print(f"{spectrum_live_time_in_s=}")
+    # spectrum_total_cps = sum(spectrum_counts) / spectrum_live_time_in_s
+    # std_dev = np.std(spectrum_counts)
+    # mean_counts = np.mean(spectrum_counts)
+    # median_counts = np.median(spectrum_counts)
+    # print(f"{std_dev=}, {mean_counts=}, {median_counts=}")
+
+    # threshold = median_counts
+
     print(f"{threshold=}")
 
     # reverse iterate list to search backwards - no zero peak to worry about, and generally should be faster.
@@ -50,10 +75,12 @@ def sanityCheckSpectrum(
             # this point should be LOWER than source voltage *almost* always. some exclusions, incl. sum peaks, but those should be niche.
             return True
         else:
+            print(f"FAILED: {source_voltage_in_kV}kV phase")
             return False
 
     else:
         # No peak above noise detected - flat spectra?
+        print(f"FAILED: {source_voltage_in_kV}kV phase")
         return False
 
 
@@ -62,33 +89,37 @@ def main():
     # assay = PDZFile("00156-REE_IDX.pdz")
     # assay = PDZFile("00007-Spectrometer Mode.pdz")
     # assay = PDZFile("00007-GeoExploration-SiO2.pdz")
-    assay = PDZFile("00279-GeoExploration-SiO2-180s.pdz")
+    # assay = PDZFile("00279-GeoExploration-SiO2-180s.pdz")
     # assay = PDZFile("00148-GeoExploration.pdz")
-    # assay = PDZFile("00020-AuPathfinder.pdz")
-    print(f"{assay.spectrum1.sourceVoltage=}")
-    print(f"{assay.spectrum1.timeLive=}")
+    assay = PDZFile("00020-AuPathfinder.pdz")
+
+    # print(f"{assay.spectrum1.sourceVoltage=}")
+    # print(f"{assay.spectrum1.timeLive=}")
     sanityCheckSpectrum(
         spectrum_counts=assay.spectrum1.counts,
         spectrum_energies=assay.spectrum1.energies,
         source_voltage_in_kV=int(assay.spectrum1.sourceVoltage),
+        spectrum_live_time_in_s=assay.spectrum1.timeLive,
     )
     try:
-        print(f"{assay.spectrum2.sourceVoltage=}")
-        print(f"{assay.spectrum2.timeLive=}")
+        # print(f"{assay.spectrum2.sourceVoltage=}")
+        # print(f"{assay.spectrum2.timeLive=}")
         sanityCheckSpectrum(
             spectrum_counts=assay.spectrum2.counts,
             spectrum_energies=assay.spectrum2.energies,
             source_voltage_in_kV=int(assay.spectrum2.sourceVoltage),
+            spectrum_live_time_in_s=assay.spectrum2.timeLive,
         )
     except AttributeError:
         print("no phase 2 present in pdz")
     try:
-        print(f"{assay.spectrum3.sourceVoltage=}")
-        print(f"{assay.spectrum3.timeLive=}")
+        # print(f"{assay.spectrum3.sourceVoltage=}")
+        # print(f"{assay.spectrum3.timeLive=}")
         sanityCheckSpectrum(
             spectrum_counts=assay.spectrum3.counts,
             spectrum_energies=assay.spectrum3.energies,
             source_voltage_in_kV=int(assay.spectrum3.sourceVoltage),
+            spectrum_live_time_in_s=assay.spectrum3.timeLive,
         )
     except AttributeError:
         print("no phase 3 present in pdz")
